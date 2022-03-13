513b253e2c48e35084cbfaa5319cd903efb1cb32
rmd3 <michaeldanjolell@gmail.com>
2022-03-13 17:43:20 -0400

    Add project files.

---
+++ D'Anjolell_7_6/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿
+namespace D_Anjolell_7_6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEntry = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblEntry = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtEntry
+            // 
+            this.txtEntry.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.txtEntry.Location = new System.Drawing.Point(432, 115);
+            this.txtEntry.Name = "txtEntry";
+            this.txtEntry.Size = new System.Drawing.Size(144, 34);
+            this.txtEntry.TabIndex = 11;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblResult.Location = new System.Drawing.Point(30, 347);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 29);
+            this.lblResult.TabIndex = 10;
+            // 
+            // lblEntry
+            // 
+            this.lblEntry.AutoSize = true;
+            this.lblEntry.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblEntry.Location = new System.Drawing.Point(196, 120);
+            this.lblEntry.Name = "lblEntry";
+            this.lblEntry.Size = new System.Drawing.Size(221, 29);
+            this.lblEntry.TabIndex = 9;
+            this.lblEntry.Text = "Enter child\'s name: ";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lblTitle.Location = new System.Drawing.Point(285, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(225, 39);
+            this.lblTitle.TabIndex = 8;
+            this.lblTitle.Text = "Name Search";
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnQuit.Location = new System.Drawing.Point(432, 226);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(125, 47);
+            this.btnQuit.TabIndex = 7;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = true;
+            this.btnQuit.Click += new System.EventHandler(this.btnQuit_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnSubmit.Location = new System.Drawing.Point(247, 226);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(125, 47);
+            this.btnSubmit.TabIndex = 6;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtEntry);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.lblEntry);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Form1";
+            this.Text = "Name Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEntry;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblEntry;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Button btnSubmit;
+    }
+}
+
---
+++ D'Anjolell_7_6/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D_Anjolell_7_6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            //Code that closes the program
+            this.Close();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //Richard D'Anjolell
+            //Try catch statement if there is an error accessing the file
+
+            try
+            {
+                //Declare variables
+                int popularName = 0;
+                const int ARRAY_SIZE = 200;
+                string[] boyLines = new string[ARRAY_SIZE];
+                string[] girlLines = new string[ARRAY_SIZE];
+                string userInput = txtEntry.Text;
+                StreamReader boyFile;
+                StreamReader girlFile;
+
+                //Open both files
+                boyFile = File.OpenText("..\\..\\BoyNames.txt");
+                girlFile = File.OpenText("..\\..\\GirlNames.txt");
+
+                //Declare the index for the arrays
+                int indexNum = 0;
+
+                //Sends names from file into an array
+                while (indexNum < boyLines.Length && !boyFile.EndOfStream)
+                {
+                    //Set array name to name in the file
+                    boyLines[indexNum] = boyFile.ReadLine();
+
+                    //Increment index counter
+                    indexNum++;
+                }
+
+                //Resets index for next while loop
+                indexNum = 0;
+
+                //Sends name from file into an array
+                while (indexNum < girlLines.Length && !girlFile.EndOfStream)
+                {
+                    //Set array name to name in the file
+                    girlLines[indexNum] = girlFile.ReadLine();
+
+                    //Increment index counter
+                    indexNum++;
+                }
+
+                //Resets index for next while loop
+                indexNum = 0;
+
+                //Finds out if the inputed name is in the list
+                while (indexNum < boyLines.Length)
+                {
+                    if (boyLines[indexNum] == userInput)
+                    {
+                        lblResult.Text = "The name you have entered (" + userInput + ") is among the most popular.";
+                        popularName++;
+                    }
+                    else if (girlLines[indexNum] == userInput)
+                    {
+                        lblResult.Text = "The name you have entered (" + userInput + ") is among the most popular.";
+                        popularName++;
+                    }
+
+                    //Increment index counter
+                    indexNum++;
+                }
+
+                //If statement in case name isn't in the list
+                if (popularName == 0)
+                {
+                    lblResult.Text = "The name you have entered (" + userInput + ") is not among the most popular.";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
